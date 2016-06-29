dd986e0d6571a7491f59b3c91fddaa63bbb6b69b
François Jacquet <francoisjacquet@users.noreply.github.com>
2016-06-29 15:51:54 -0500

    Update config.inc.php file (2016)

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -1,23 +1,88 @@
 <?php
-if(!defined('CONFIG_INC'))
-{
-  define('CONFIG_INC',1);
-  
-  // PostgreSQL Database Setup
-  $DatabaseServer = getenv('ROSARIODB_PORT_5432_TCP_ADDR');
-  $DatabaseUsername = 'postgres';
-  // $DatabasePassword = '';
-  $DatabaseName = 'rosariosis';
-  $DatabasePort = getenv('ROSARIODB_PORT_5432_TCP_PORT');
+/**
+ * The base configurations of RosarioSIS
+ *
+ * You can find more information in the INSTALL.md file
+ *
+ * @package RosarioSIS
+ */
 
-  // Server Names and Paths
-  $RosarioPath = dirname(__FILE__).'/';
-  $pg_dumpPath = '/usr/bin/pg_dump'; // Specify the path to the database dump utility for this server.
-  $wkhtmltopdfPath = '/usr/bin/wkhtmltopdf'; // empty string means wkhtmltopdf will not be called and reports will be rendered in html instead of pdf
+/**
+ * PostgreSQL Database Settings
+ *
+ * You can get this info from your web host
+ */
 
-  $DefaultSyear = '2014'; // Default school year, should match the database to be able to login
-  $RosarioNotifyAddress = getenv('ROSARIOSIS_ADMIN_EMAIL'); // email address to send error and new administrator notifications
-  $RosarioLocales = array('en_US.utf8'); // Add other languages you want to support here, ex: array('en_US.utf8', 'fr_FR.utf8', 'es_ES.utf8');
+// Database server hostname: use localhost if on same server.
+$DatabaseServer = getenv( 'ROSARIODB_PORT_5432_TCP_ADDR' );
 
-}
-?>
+// Database username.
+$DatabaseUsername = 'postgres';
+
+// Database password.
+//$DatabasePassword = 'password_here';
+
+// Database name.
+$DatabaseName = 'rosariosis';
+
+// Database port: default is 5432.
+$DatabasePort = getenv( 'ROSARIODB_PORT_5432_TCP_PORT' );
+
+
+/**
+ * Paths
+ */
+
+/**
+ * Full path to the PostrgeSQL database dump utility for this server
+ *
+ * @example /usr/bin/pg_dump
+ * @example C:/Progra~1/PostgreSQL/bin/pg_dump.exe
+ */
+$pg_dumpPath = '/usr/bin/pg_dump';
+
+/**
+ * Full path to wkhtmltopdf binary file
+ *
+ * An empty string means wkhtmltopdf will not be called
+ * and reports will be rendered in HTML instead of PDF
+ *
+ * @link http://wkhtmltopdf.org
+ *
+ * @example /usr/local/bin/wkhtmltopdf
+ * @example C:/Progra~1/wkhtmltopdf/bin/wkhtmltopdf.exe
+ */
+$wkhtmltopdfPath = '/usr/local/bin/wkhtmltopdf';
+
+
+/**
+ * Default school year
+ *
+ * Do not change on install
+ * Change after rollover
+ * Should match the database to be able to login
+ */
+$DefaultSyear = '2016';
+
+
+/**
+ * Notify email address
+ * where to send error and new administrator notifications
+ *
+ * Leave empty to not receive email notifications
+ */
+$RosarioNotifyAddress = getenv( 'ROSARIOSIS_ADMIN_EMAIL' );
+
+
+/**
+ * Locales
+ *
+ * Add other languages you want to support here
+ *
+ * @see locale/ folder
+ *
+ * For American, French and Spanish:
+ *
+ * @example array( 'en_US.utf8', 'fr_FR.utf8', 'es_ES.utf8' );
+ */
+$RosarioLocales = array( 'en_US.utf8' );
